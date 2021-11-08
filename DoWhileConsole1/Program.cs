634c7116a49f5ab68ca19d194fa7157e7387c6d1
Daria Mazurenko <kn212_mdd@student.ztu.edu.ua>
2021-11-08 22:01:32 +0200

    add WhileConsole3 and DoWhileConsole1

---
+++ DoWhileConsole1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int summa = 0;
+            do
+            {
+                Console.Write("n = ");
+                n = int.Parse(Console.ReadLine());
+                summa += n;
+            } while (n != 0);
+            Console.Write($"Summa = {summa}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileConsole3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileConsole3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p;
+            Console.Write("p = ");
+            p = double.Parse(Console.ReadLine());
+            int k = 1;
+            double d = 10, summa = 10;
+            while(summa <= 200)
+            {
+                ++k;
+                d += d * p / 100;
+                summa += d;
+            }
+            Console.WriteLine($"summa =\t{summa}");
+            Console.WriteLine($"kilkist' dniv =\t{k}");
+            Console.ReadKey();
+        }
+    }
+}
