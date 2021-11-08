4096cbed2cedaeb915ab52bb31e264a87e42483e
Daria Mazurenko <kn212_mdd@student.ztu.edu.ua>
2021-11-08 21:50:23 +0200

    add ForConsole5

---
+++ ForConsole5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForConsole5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, Fact = 1, result = 1;
+            Console.Write("a = ");
+            a = double.Parse(Console.ReadLine());
+            if (a == 0)
+                Console.Write("Error");
+            for(double i = 1; i <= a; i++)
+            {
+                result += Fact *= i;
+            }
+            Console.Write(result);
+            Console.ReadKey();
+
+        }
+    }
+}
