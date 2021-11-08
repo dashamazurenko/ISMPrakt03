d34e3bfa8a3c25a5867a1f8d89a29388eaae117a
Daria Mazurenko <kn212_mdd@student.ztu.edu.ua>
2021-11-08 21:55:01 +0200

    add WhileConsole1 and WhileConsole2

---
+++ WhileConsole1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, result = 0;
+            Console.Write("a = ");
+            a = double.Parse(Console.ReadLine());
+            int i = 1;
+            while(i <= a)
+            {
+                result += Math.Pow(i, a - i + 1);
+                i++;
+            }
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileConsole2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, k = 0;
+            Console.Write("a = ");
+            a = double.Parse(Console.ReadLine());
+            while (Math.Pow(3, k) < a)
+            {
+                k++;
+            }
+            Console.Write(k);
+            Console.ReadKey();
+        }
+    }
+}
